d7d8319e9c6ba311638303a8632eea99e6f526c9
xGabrielR <gabrielrichter2020@gmail.com>
2022-01-24 13:52:47 -0300

    Scripts Update

--- scripts/statistics.jl
+++ scripts/statistics.jl
@@ -38,4 +38,32 @@ function pearson_corr(x, y)
     return compute_r(x, y)
 end
 
-r = pearson_corr(x, y)
+
+"""
+    cramer_corr function computes X, Y One Dims vector or matrix and return correlation of the two vectors, used on categorical data.
+"""
+function cramer_corr(x, y)
+    function check_array(x, y)
+        if isempty(x) || isempty(y)
+            return @error("Empty Array")
+        elseif !(length(x) == length(y))
+            return @error("Arrays are not of the same length")
+        else
+        end
+    end;
+    function compute_v( x, y )
+        cm = freqtable(x, y)
+        n  = sum(cm)
+        r, k = size(cm)
+
+        chi2 = ChisqTest( cm ).stat
+        chi2corr = max( 0, chi2 - ((( k-1 ) * ( r-1 )) / ( n-1 ) ))
+
+        kcorr = k - ((( k-1 )^2) / ( n-1 ))
+        rcorr = r - ((( r-1 )^2) / ( n-1 ))
+
+        return sqrt( ( chi2corr / n ) / ( min( kcorr - 1, rcorr - 1 ) ) )
+    end;
+    check_array(x, y)
+    return compute_v(x, y)
+end;
